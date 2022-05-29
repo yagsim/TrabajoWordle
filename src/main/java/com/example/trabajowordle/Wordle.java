21f219ca366d91b90836c6aab0a3baef51cf129f
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-05-29 21:16:22 +0200

    1

--- src/main/java/com/example/trabajowordle/Wordle.java
+++ src/main/java/com/example/trabajowordle/Wordle.java
@@ -12,7 +12,7 @@ public class Wordle extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Wordle.class.getResource("wordle2.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 697, 834);
         stage.setTitle("Wordle");
         stage.setScene(scene);
         stage.show();
